090855f71ec8d9be26af7e217baf0a4b77a6b1f3
unknown <farzanaka22@gmail.com>
2023-05-11 13:24:59 +0530

    initial commit

---
+++ cal.js
@@ -0,0 +1,16 @@
+function displaydata(n)
+{
+    result.value=result.value+n
+}
+function evaluateOpr()
+{
+    result.value=eval(result.value)
+}
+function allClear()
+{
+    result.value=""
+}
+function backSpace()
+{
+    result.value=result.value.slice(0,1)
+}
